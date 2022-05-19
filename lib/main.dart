f18c1852aca28662e3d678c94db63f0e90a5b9ca
StartUpTech21 <78974051+StartUpTech21@users.noreply.github.com>
2022-05-19 10:22:38 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+// ignore: import_of_legacy_library_into_null_safe, unused_import
+import 'package:permission/permission.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Flutter Google Maps Demo',
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Google Maps Flutter'),
+      ),
+      drawer: const DrawerScreen(),
+      body: const GoogleMap(
+        mapType: MapType.normal,
+        initialCameraPosition: CameraPosition(
+          target: LatLng(-1.8165475, 109.9822499),
+          zoom: 14.0,
+        ),
+      ),
+    );
+  }
+}
+
+class DrawerScreen extends StatefulWidget {
+  const DrawerScreen({Key? key}) : super(key: key);
+
+  @override
+  State<DrawerScreen> createState() => DrawerScreenState();
+}
+
+class DrawerScreenState extends State<DrawerScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: <Widget>[
+          const UserAccountsDrawerHeader(
+            accountName: Text("Widi Arrohman"),
+            currentAccountPicture: CircleAvatar(
+                backgroundImage: AssetImage("assets/img/widi.jpg")),
+            accountEmail: Text("widiarrohman1234@gmail.com"),
+          ),
+          DrawerListTile(
+            iconData: Icons.group,
+            title: "New Group",
+            onTilePressed: () {},
+          ),
+          DrawerListTile(
+            iconData: Icons.lock,
+            title: "New Secret Group",
+            onTilePressed: () {},
+          ),
+          DrawerListTile(
+            iconData: Icons.notifications,
+            title: "New Channel Chat",
+            onTilePressed: () {},
+          ),
+          DrawerListTile(
+            iconData: Icons.contacts,
+            title: "Contacs",
+            onTilePressed: () {},
+          ),
+          DrawerListTile(
+            iconData: Icons.bookmark,
+            title: "Saved Message",
+            onTilePressed: () {},
+          ),
+          DrawerListTile(
+            iconData: Icons.phone,
+            title: "Calls",
+            onTilePressed: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class DrawerListTile extends StatelessWidget {
+  const DrawerListTile(
+      {Key? key,
+      required this.iconData,
+      required this.title,
+      required this.onTilePressed})
+      : super(key: key);
+  final IconData? iconData;
+  final String? title;
+  final VoidCallback? onTilePressed;
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      onTap: onTilePressed,
+      dense: true,
+      leading: Icon(iconData),
+      title: Text(
+        title!,
+        style: const TextStyle(fontSize: 16),
+      ),
+    );
+  }
+}
